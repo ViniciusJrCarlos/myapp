7f59410fd240c424e2ff3256db55645150ad22b9
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-08-18 01:45:17 -0300

    criando rotas e menu de pages do site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //import logo from './logo.svg';
+/*
 import './App.css';
 import styled from 'styled-components';
 
@@ -29,23 +30,73 @@ const Titulo2 = styled.h1`
 
 `;
 
+*/
+import React, { Component } from 'react';
+import 'primereact/resources/primereact.min.css';
+import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import { Menubar } from 'primereact/menubar';
+import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 
+const DivMain=styled.h1`
 
-function App() {
-  return (
-   
-    <div className="App">
-      <header className="App-header">
-        <Titulo>   Meu Site </Titulo>
-        <Subtitulo>Subtitulo</Subtitulo>
-        <Artigo>   Lorem ipsum </Artigo>
-        <Titulo2>  Segunda Parte</Titulo2>
-        <Artigo>    Lorem ipsum </Artigo>
-      </header>
-    </div>
-  
-  );
+  color: black;
+  font-size:10px;+
+
+`;
+
+class App extends Component {
+
+      render() {
+
+        const menuitems = [
+
+          {
+
+            label: 'Home',
+            command:() => this.props.history.push('/')
+
+          },
+          {
+
+            label:'Sobre',
+            command:() => this.props.history.push('/sobre')
+
+
+          },
+          {
+
+            label:'Contato'
+            command:() => this.props.history.push('/contato')
+          }
+
+        ];
+
+      
+
+        return (
+          
+          <div className="App">
+
+              <Menubar model={menuitems}/>
+                <Header />
+                  <DivMain>
+                      <main>
+                        <div className="content" id="content">
+                            {this.props.children}
+                        </div>
+                      </main>
+                  </DivMain>
+              <Footer />
+            
+            
+          </div>
+        
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +16,35 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Home from './pages/home';
+import Sobre from './pages/sobre';
+import Contato from './pages/contato';
+
+
+//import reportWebVitals from './reportWebVitals';
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+    (
+
+      <Router>
+        <App>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/sobre" component={Sobre} />
+            <Route path="/contato" component={Contato} />
+          </Switch>
+        </App>
+      </Router>
+
+    ),
+    document.getElementById('root')
+
+);
