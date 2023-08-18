b47d2e07e0743ef156e065e59e3c36e116b45d97
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-08-17 23:32:31 -0300

    enviando atualizacao do app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Titulo = styled.h1`
+
+  color: #31e636;
+  text-decoration: underline;
+
+`;
+
+const Subtitulo = styled.h2`
+
+  color: green;
+
+`;
+
+const Artigo = styled.section`
+
+  color: #9393e2;
+
+`;
+
+const Titulo2 = styled.h1`
+
+  color: blue;
+  font-size: 25px;
+
+
+`;
+
+
+
 
 function App() {
   return (
+   
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Titulo>   Meu Site </Titulo>
+        <Subtitulo>Subtitulo</Subtitulo>
+        <Artigo>   Lorem ipsum </Artigo>
+        <Titulo2>  Segunda Parte</Titulo2>
+        <Artigo>    Lorem ipsum </Artigo>
       </header>
     </div>
+  
   );
 }
 
